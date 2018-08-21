27ba0e863a1a7c747d08320fd1c637c0c4770a7e
YuliyaNaychuk <yuliagudzyk77@gmail.com>
2018-08-21 23:53:16 +0300

    FE_9_7_homework_control-flow added

--- FE_9_7_homework_control-flow/homework/src/js/task1.js
+++ FE_9_7_homework_control-flow/homework/src/js/task1.js
@@ -1,1 +1,22 @@
-// Your code goes here
+const userName = prompt('Login:');
+
+if (userName === 'User'){
+    const pass = prompt('Password:');
+
+    if (pass === 'SuperUser'){
+    alert(new Date().getHours() < 20 ? 'Good evening!' : 'Good day!');
+    }else if(!pass){
+    alert('Canceled.');
+    }else{
+    alert('Wrong password');
+    }
+}else {
+
+    if (!userName){
+        alert('Canceled.');
+    }else if(userName.length < 4){
+        alert(`I don't know any users having name length less than 4 symbols`);
+    }else{
+        alert(`I don’t know you`);    
+    }
+}
--- FE_9_7_homework_control-flow/homework/src/js/task2.js
+++ FE_9_7_homework_control-flow/homework/src/js/task2.js
@@ -1,1 +1,65 @@
-// Your code goes here
+const rules = {
+    prizeMult: 3,
+    rangeMult: 2,
+    totalAtts: 3
+};
+Object.freeze(rules);
+
+const gameInfo = {
+    prize: 10,
+    maxRangeVal: 5,
+    totalPrize: 0
+};
+
+const info = (maxRangNum, attsLeft, totalPrize, possPrizeCurrAtt) => `
+	Enter a number from 0 to ${maxRangNum}
+	Attempts left: ${attsLeft}
+	Total prize: ${totalPrize}$
+	Possible prize on current attempt: ${possPrizeCurrAtt}$`;
+
+function getrandNum(maxRangNum) {
+    return Math.floor(Math.random() * ++maxRangNum);
+}
+
+let isGameOn = false;
+
+if(confirm('Do you want to play a game?')){
+    do {
+        isGameOn = false;
+
+        const randNum = getrandNum(gameInfo.maxRangeVal);
+
+        for(
+            let currAtt = 0, possPrizeCurrAtt = gameInfo.prize;
+            currAtt < rules.totalAtts;
+            currAtt++, possPrizeCurrAtt = Math.floor(possPrizeCurrAtt / 2)
+        ) {
+            const userInput = +prompt(
+                info(
+                    gameInfo.maxRangeVal,
+                    rules.totalAtts - currAtt,
+                    gameInfo.totalPrize,
+                    possPrizeCurrAtt
+                )
+            );
+
+            if (userInput === randNum) {
+                gameInfo.totalPrize += possPrizeCurrAtt;
+                if(confirm(`
+                    Congratulation! Your prize is: ${gameInfo.totalPrize}$ 
+                    Do you want to continue?`)
+                ) {
+                    gameInfo.prize *= rules.prizeMult;
+                    gameInfo.maxRangeVal *= rules.rangeMult;
+                    isGameOn = true;
+                    break;
+                }
+
+            } else if (currAtt === rules.totalAtts) {
+                alert(`Thank you for a game. Your prize is: ${gameInfo.totalPrize}$`);
+            }
+        }
+    } while (isGameOn);
+}else{
+    alert('You did not become a millionaire, but can.');
+}
